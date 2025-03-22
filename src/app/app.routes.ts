80a7e5472546a8c324890d038958316abceed420
unknown <harshadwagh535@gmail.com>
2025-03-22 14:45:11 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+    { 
+        path: 'Home',
+        loadComponent: ()=> import('./home/home.component').then(m => m.HomeComponent)
+    },
+    { 
+        path: 'About',
+        loadComponent: ()=> import('./about/about.component').then(m => m.AboutComponent)
+    },
+  ];
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  constructor( private route:Router){}
+
+  nevigate(input:any){
+    if(input == 'About'){
+      this.route.navigate(['/About']);
+
+    }
+    else{
+
+    }
+  }
+
+}
